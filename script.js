e13400242c0a7825dbc2420217e1da3d6de19752
mistiq89 <97790850+mistiq89@users.noreply.github.com>
2022-02-24 13:42:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerText="If you deposit <mark>"+ principal+"</mark>,<br/>"+
-    "at an interest rate of <mark>" + rate + "</mark>,<br/>"+
-    "You will receive an amount of <mark>"+ interest + "</mark>,<br/>"+
-    "in the year <mark>"+ year +"</mark>";
+    document.getElementById("result").innerText="If you deposit "+ principal+",<br/>"+
+    "at an interest rate of " + rate + ",<br/>"+
+    "You will receive an amount of "+ interest + ",<br/>"+
+    "in the year "+ year;
     
 }
 
